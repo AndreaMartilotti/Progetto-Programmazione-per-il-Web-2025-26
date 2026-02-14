c65f817e61e3ec3bc4ce979f7db24fd2598ec459
caso.exe <valentinocaso@gmail.com>
2026-02-14 01:09:58 +0100

    Update UserRoleController.php

--- ProgettoPPW2526/app/Http/Controllers/UserRoleController.php
+++ ProgettoPPW2526/app/Http/Controllers/UserRoleController.php
@@ -24,18 +24,19 @@ class UserRoleController extends Controller
             foreach ($rolesFromForm as $userId => $newRole) {
                 \App\Models\User::where('id',$userId)->update(['role'=>$newRole]);
             }
-        }//inserire filtri di ricerca e notifica di success sulla schermata
+        }
         return back()->with('success','Aggiornamento ruolo effettuato');
     }
 
     public function search(Request $request){
         //recupero dalla value request 'search'
         $search=$request->input('search');
+        //ricerca con valori simili all'interno della tabella
         if ($search) {
-           $users=User::where('id', 'LIKE', "%$search")
-                       ->orWhere('name', 'LIKE', "%$search")
-                       ->orWhere('email', 'LIKE', "%$search")
-                       ->orWhere('role', 'LIKE', "%$search")
+           $users=User::where('id', 'LIKE', "%$search%")
+                       ->orWhere('name', 'LIKE', "%$search%")
+                       ->orWhere('email', 'LIKE', "%$search%")
+                       ->orWhere('role', 'LIKE', "%$search%")
                         ->get();
         } else {
             $users=User::all();
